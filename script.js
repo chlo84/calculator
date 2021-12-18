9f19736092d16ee937d2d1a54f736f1ea43fff54
Chloe <chloe84@msn.com>
2021-12-18 12:34:05 -0800

    added more content to the project

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log('This is where the Magic Happens')
+
+const output = document.querySelector('.output')
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', () =>{
+    console.log('button was clicked')
+})
