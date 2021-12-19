35a21543ddaeb61af0ceb2ddc4ef75667c80cc08
Chloe <chloe84@msn.com>
2021-12-18 16:48:12 -0800

    updated my styles and html files

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ const output = document.querySelector('.output')
 
 const button = document.querySelector('button');
 
-button.addEventListener('click', () =>{
-    console.log('button was clicked')
-})
+const buttons = document.querySelectorAll('button')
+
+
+
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener('click', () =>{
+        console.log('button was clicked')
+        output.innerText += buttons[i].innerText;
+    });
+}
+console.log(buttons)
