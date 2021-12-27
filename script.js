cb4e7b3b1ce8d514903f6e3202210c88a388b009
Chloe <chloe84@msn.com>
2021-12-27 14:40:30 -0800

    updated html and js added functionality to calculator

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 console.log('This is where the Magic Happens')
 
 const output = document.querySelector('.output')
-
-const button = document.querySelector('button');
-
-const buttons = document.querySelectorAll('button')
-
-
+const buttons = document.querySelectorAll('.number')
+const clear = document.querySelector('#clear')
+const equal = document.querySelector('#equals')
 
 for(let i = 0; i < buttons.length; i++){
     buttons[i].addEventListener('click', () =>{
@@ -16,3 +13,15 @@ for(let i = 0; i < buttons.length; i++){
 }
 console.log(buttons)
 
+function clearAll(){
+    output.innerText = ''
+}
+clear.addEventListener('click', clearAll)
+
+// write a function that calculates the string that is on the screen.
+// Add an EventListener on the equal button that runs the calculation function.
+
+function calculates(){
+    output.innerText = ''
+}
+clear.addEventListener('click', calculates)
